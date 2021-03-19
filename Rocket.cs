a6d3ab41e263e32da72c7f1d06b4e246099ec9a6
NotEvenAiming <45467467+NotEvenAiming@users.noreply.github.com>
2021-03-19 10:52:36 +0100

    Added Rocket script and follow script

---
+++ Rocket.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public float rotationSpeed = 0;
+    private bool Thrust = false;
+
+   
+    void Start()
+    {
+        
+    }
+
+    void CheckThrustKeyPress()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Thrust = true;
+        }else
+        {
+            Thrust = false;
+        }
+    }
+
+    void CheckRotationKeyPress()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * rotationSpeed);
+        }
+    }
+
+    void ApplyRocketThrust()
+    {
+        if (Thrust)
+        {
+            Vector3 force = transform.up * 4;
+
+            GetComponent<Rigidbody>().AddForce(force);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckThrustKeyPress();
+        CheckRotationKeyPress();
+        ApplyRocketThrust();
+    }
+}
